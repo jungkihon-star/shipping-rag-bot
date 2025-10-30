07a3a76d6ed3f83769e7be045e21e52d0a81d581
jungkihon-star <jung.kihon@gmail.com>
2025-10-30 10:21:39 +0800

    Update ping.py

--- api/ping.py
+++ api/ping.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
-
 app = FastAPI()
-
-@app.get("/")
+@app.get("/") # Vercel이 /api/ping/ 으로 라우팅할 것임
 def ping():
     return PlainTextResponse("ok")
