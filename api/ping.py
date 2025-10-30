c9e4bd1d6eb7867bb5bb2e754e7faa6a0ef7b52f
jungkihon-star <jung.kihon@gmail.com>
2025-10-30 10:34:10 +0800

    Update ping.py

--- api/ping.py
+++ api/ping.py
@@ -1,6 +1,7 @@
+# api/ping.py 파일 내용
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 app = FastAPI()
-@app.get("/") # Vercel이 /api/ping/ 으로 라우팅할 것임
+@app.get("/") # Vercel의 Rewrites를 통해 이 경로로 요청이 전달될 것임
 def ping():
     return PlainTextResponse("ok")
